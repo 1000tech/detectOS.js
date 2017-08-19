9028c586548c6dac5f4211886ddeea1f05d4217e
Vadim Zaretskiy <vadizar@mobidevices.ru>
2017-08-19 21:08:39 +0300

    Add browsers detect

--- detectOS.js
+++ detectOS.js
@@ -1,11 +1,14 @@
 var Detect = {
     init: function () {
+        this.browser = this.searchString(this.dataBrowser);
+        this.version = this.searchVersion(navigator.userAgent) || this.searchVersion(navigator.appVersion);
         this.OS = this.searchString(this.dataOS);
     },
     searchString: function (data) {
         for (var i=0;i<data.length;i++) {
             var dataString = data[i].string;
             var dataProp = data[i].prop;
+            this.versionSearchString = data[i].versionSearch || data[i].identity;
             if (dataString) {
                 if (dataString.indexOf(data[i].subString) != -1)
                     return data[i].identity;
@@ -14,7 +17,80 @@ var Detect = {
                 return data[i].identity;
         }
     },
-    dataOS : [
+    searchVersion: function (dataString) {
+        var index = dataString.indexOf(this.versionSearchString);
+        if (index == -1) return;
+        return parseFloat(dataString.substring(index+this.versionSearchString.length+1));
+    },
+    dataBrowser: [
+        {
+            string: navigator.userAgent,
+            subString: "Chrome",
+            identity: "Chrome"
+        },
+        { string: navigator.userAgent,
+            subString: "OmniWeb",
+            versionSearch: "OmniWeb/",
+            identity: "OmniWeb"
+        },
+        {
+            string: navigator.vendor,
+            subString: "Apple",
+            identity: "Safari",
+            versionSearch: "Version"
+        },
+        {
+            prop: window.opera,
+            identity: "Opera",
+            versionSearch: "Version"
+        },
+        {
+            string: navigator.vendor,
+            subString: "iCab",
+            identity: "iCab"
+        },
+        {
+            string: navigator.vendor,
+            subString: "KDE",
+            identity: "Konqueror"
+        },
+        {
+            string: navigator.userAgent,
+            subString: "Firefox",
+            identity: "Firefox"
+        },
+        {
+            string: navigator.vendor,
+            subString: "Camino",
+            identity: "Camino"
+        },
+        {
+            /* For Newer Netscapes (6+) */
+            string: navigator.userAgent,
+            subString: "Netscape",
+            identity: "Netscape"
+        },
+        {
+            string: navigator.userAgent,
+            subString: "MSIE",
+            identity: "Internet Explorer",
+            versionSearch: "MSIE"
+        },
+        {
+            string: navigator.userAgent,
+            subString: "Gecko",
+            identity: "Mozilla",
+            versionSearch: "rv"
+        },
+        {
+            /* For Older Netscapes (4-) */
+            string: navigator.userAgent,
+            subString: "Mozilla",
+            identity: "Netscape",
+            versionSearch: "Mozilla"
+        }
+    ],
+    dataOS: [
         {
             string: navigator.platform,
             subString: 'Win',
@@ -55,4 +131,6 @@ var Detect = {
 
 Detect.init();
 
-console.log("We know your OS – it's " + Detect.OS + ". We know everything about you.");
+console.log("We know your browser – it's " + Detect.browser + " " + Detect.version);
+console.log("We know your OS – it's " + Detect.OS);
+console.log("We know everything about you.");
